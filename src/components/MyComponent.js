05d5b4bc7b88319cb2d0c7d2eefb9a2231c27e62
unknown <hiepga11042003@gmail.com>
2025-01-26 21:33:34 +0700

    react component

---
+++ src/components/MyComponent.js
@@ -0,0 +1,19 @@
+// class component
+// function component
+
+
+import React from 'react';
+
+class MyComponent extends React.Component {
+    
+    render() {
+        return (
+            <div>
+                my first Component
+                {Math.random()}
+            </div>
+        )
+    }
+}
+
+export default MyComponent
